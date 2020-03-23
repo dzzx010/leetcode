8ea94d76e4f393ec1c8530104ac98b15c071545c
dzzx010 <dzzx010@126.com>
2020-03-23 10:25:25 +0800

    70

--- 70.爬楼梯.js
+++ 70.爬楼梯.js
@@ -9,8 +9,16 @@
  * @param {number} n
  * @return {number}
  */
+var temp = {}
+temp[1] = 1
+temp[2] = 2
 var climbStairs = function(n) {
-
+    
+    if(temp[n]) return temp[n]
+    temp[n-1] = climbStairs(n-1)
+    temp[n-2] = climbStairs(n-2)
+    return temp[n-1] + temp[n-2]
 };
+
 // @lc code=end
 
